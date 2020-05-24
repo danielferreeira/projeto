9defec5898ec3180f180595d7d8cf817f8d6be0a
Daniel <danielferreiramh@gmail.com>
2020-05-24 18:26:37 -0300

    Subindo o secret

---
+++ backend/src/config/secret.js
@@ -0,0 +1,8 @@
+module.exports = {
+    PORT: 3333,
+    DATABASE_NAME: 'projeto',
+    DATABASE_USER: 'postgres',
+    DATABASE_PASSWORD: 'postgres',
+    DATABASE_HOST: 'localhost',
+    DATABASE_DIALECT: 'postgres'
+}
